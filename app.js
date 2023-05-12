ec870f33e62cf7e780e4176a522257a4ab3893eb
Vaibhav <vs630916@gmail.com>
2023-05-12 21:07:34 +0530

    mongoDB and setting up with it

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
 require("dotenv").config();
 
+//app
+const app = express();
+
+// DATABASE CONNECTION
+const DB = process.env.DATABASE;
+mongoose.connect(DB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+    console.log("Database connected.");
+}).catch((err) => {
+    console.log("Database error");
+});
+
+
+//routes
 app.get("/", (req, res) => {
   res.send("hello from node Ahhyeah");
 });
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server is running on the port ${port}`);
