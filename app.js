540ad583692967731f6064e0e627dde2bb799eb3
Vaibhav <vs630916@gmail.com>
2023-05-13 20:51:43 +0530

    Structuring our project

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const userRoutes = require("./routes/user");
 
 //DOTENV Config
 require("dotenv").config();
@@ -9,17 +10,17 @@ const app = express();
 
 // DATABASE CONNECTION
 const DB = process.env.DATABASE;
-mongoose.connect(DB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+mongoose
+  .connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
     console.log("Database connected.");
-}).catch((err) => {
+  })
+  .catch((err) => {
     console.log("Database error");
-});
+  });
 
-
-//routes
-app.get("/", (req, res) => {
-  res.send("hello from node ");
-});
+//middleware
+app.use("/api", userRoutes);
 
 const port = process.env.PORT || 8000;
 
