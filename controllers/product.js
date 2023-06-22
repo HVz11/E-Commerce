b85384aa3e8f8ee805738f1c9f2b1ca731e72de7
Vaibhav <vs630916@gmail.com>
2023-06-22 21:28:23 +0530

    ProductById

--- controllers/product.js
+++ controllers/product.js
@@ -3,6 +3,24 @@ const formidable = require("formidable-serverless");
 const fs = require("fs");
 const _ = require("lodash");
 const { errorHandler } = require("../helpers/dbErrorHandler");
+const product = require("../models/product");
+
+exports.productById = (req, res, next) => {
+  Product.findById(id).exec(() => {
+    if (err || !product) {
+      return res.status(400).json({
+        error: "Product not found",
+      });
+    }
+    req.product = product;
+    next();
+  });
+};
+
+exports.read = (req, res) => {
+  req.product.photo = undefined;
+  return res.json(req.product);
+};
 
 exports.create = (req, res) => {
   let form = new formidable.IncomingForm();
--- routes/product.js
+++ routes/product.js
@@ -5,8 +5,10 @@ const { create } = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
+router.get("/product/:productId", read);
 router.post("/product/create/:userId", requireSignin, isAdmin, isAdmin, create);
 
 router.param("userId", userById);
+router.param("productId", productById);
 
 module.exports = router;
