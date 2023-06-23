a30867a59926546f12710c5ba268c28b7e4b4164
Vaibhav <vs630916@gmail.com>
2023-06-23 19:45:17 +0530

    Product delete method

--- routes/product.js
+++ routes/product.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-const { create } = require("../controllers/product");
+const { create, read, productById } = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
+const { remove } = require("lodash");
 
 router.get("/product/:productId", read);
-router.post("/product/create/:userId", requireSignin, isAdmin, isAdmin, create);
+router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);
+router.delete(
+  "/product/:productId/:userId",
+  requireSignin,
+  isAuth,
+  isAdmin,
+  remove
+);
 
 router.param("userId", userById);
 router.param("productId", productById);
