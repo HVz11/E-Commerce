358028fed83a60858c522473670760c264b409df
Vaibhav <vs630916@gmail.com>
2023-06-24 18:04:29 +0530

    cors package

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const expressValidator = require("express-validator");
+const cors = require("cors");
 
 //DOTENV Config
 require("dotenv").config();
@@ -33,6 +34,7 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
+app.use(cors());
 
 //Routes middleware
 app.use("/api", authRoutes);
